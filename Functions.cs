d6fcf1f53888d5eec8196eaba2dbb3853534d3bf
Ottermandias <nickroederspam+git1@gmail.com>
2022-05-18 23:02:47 +0200

    Add Base64 functions.

--- Functions.cs
+++ Functions.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace OtterGui;
 
@@ -64,4 +68,52 @@ public static class Functions
         "PB",
         "EB",
     };
+
+    // Compress any type to a base64 encoding of its compressed json representation, prepended with a version byte.
+    // Returns an empty string on failure.
+    public static unsafe string ToCompressedBase64<T>(T data, byte version)
+    {
+        try
+        {
+            var       json             = JsonConvert.SerializeObject(data, Formatting.None);
+            var       bytes            = Encoding.UTF8.GetBytes(json);
+            using var compressedStream = new MemoryStream();
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            {
+                zipStream.Write(new ReadOnlySpan<byte>(&version, 1));
+                zipStream.Write(bytes, 0, bytes.Length);
+            }
+
+            return Convert.ToBase64String(compressedStream.ToArray());
+        }
+        catch
+        {
+            return string.Empty;
+        }
+    }
+
+    // Decompress a base64 encoded string to the given type and a prepended version byte if possible.
+    // On failure, data will be default and version will be byte.MaxValue.
+    public static byte FromCompressedBase64<T>(string base64, out T? data)
+    {
+        var version = byte.MaxValue;
+        try
+        {
+            var       bytes            = Convert.FromBase64String(base64);
+            using var compressedStream = new MemoryStream(bytes);
+            using var zipStream        = new GZipStream(compressedStream, CompressionMode.Decompress);
+            using var resultStream     = new MemoryStream();
+            zipStream.CopyTo(resultStream);
+            bytes   = resultStream.ToArray();
+            version = bytes[0];
+            var json = Encoding.UTF8.GetString(bytes, 1, bytes.Length - 1);
+            data = JsonConvert.DeserializeObject<T>(json);
+        }
+        catch
+        {
+            data = default;
+        }
+
+        return version;
+    }
 }
