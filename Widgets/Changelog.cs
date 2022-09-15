2b4b78b03d679144440d35b30830608b6d2bcb79
Ottermandias <nickroederspam+git1@gmail.com>
2022-09-15 18:31:09 +0200

    Add some options to changelog processing.

--- Widgets/Changelog.cs
+++ Widgets/Changelog.cs
@@ -11,28 +11,47 @@ using OtterGui.Raii;
 
 namespace OtterGui.Widgets;
 
+public enum ChangeLogDisplayType
+{
+    New,
+    HighlightOnly,
+    Never,
+}
+
 public sealed class Changelog : Window
 {
+    public static string ToName(ChangeLogDisplayType type)
+    {
+        return type switch
+        {
+            ChangeLogDisplayType.New           => "Show New Changelogs (Recommended)",
+            ChangeLogDisplayType.HighlightOnly => "Only Show Important Changelogs",
+            ChangeLogDisplayType.Never         => "Never Show Changelogs (Dangerous)",
+            _                                  => string.Empty,
+        };
+    }
+
     public const int FreshInstallVersion = int.MaxValue;
 
     public const uint DefaultHeaderColor    = 0xFF60D0D0;
     public const uint DefaultHighlightColor = 0xFF6060FF;
 
-    private readonly Func<int>                   _getLastVersion;
-    private readonly Action<int>                 _setLastVersion;
-    private readonly List<(string, List<Entry>)> _entries = new();
+    private readonly Func<(int, ChangeLogDisplayType)> _getConfig;
+    private readonly Action<int, ChangeLogDisplayType> _setConfig;
 
-    private int _lastVersion;
+    private readonly List<(string Title, List<Entry> Entries, bool HasHighlight)> _entries = new();
+
+    private int                  _lastVersion;
+    private ChangeLogDisplayType _displayType;
 
     public uint HeaderColor { get; set; } = DefaultHeaderColor;
     public bool ForceOpen   { get; set; } = false;
 
-    public Changelog(string label, Func<int> getLastVersion, Action<int> setLastVersion)
-        : base(label, ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize,
-            true)
+    public Changelog(string label, Func<(int, ChangeLogDisplayType)> getConfig, Action<int, ChangeLogDisplayType> setConfig)
+        : base(label, ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize, true)
     {
-        _getLastVersion    = getLastVersion;
-        _setLastVersion    = setLastVersion;
+        _getConfig         = getConfig;
+        _setConfig         = setConfig;
         Position           = null;
         RespectCloseHotkey = false;
         ShowCloseButton    = false;
@@ -40,15 +59,35 @@ public sealed class Changelog : Window
 
     public override void PreOpenCheck()
     {
-        _lastVersion = _getLastVersion();
+        (_lastVersion, _displayType) = _getConfig();
+        if (ForceOpen)
+        {
+            IsOpen = true;
+            return;
+        }
+
         if (_lastVersion == FreshInstallVersion)
         {
             IsOpen = false;
-            _setLastVersion(_entries.Count);
+            _setConfig(_entries.Count, _displayType);
+            return;
         }
-        else
+
+        switch (_displayType)
         {
-            IsOpen = ForceOpen || _lastVersion < _entries.Count;
+            case ChangeLogDisplayType.New:
+                IsOpen = _lastVersion < _entries.Count;
+                break;
+            case ChangeLogDisplayType.HighlightOnly:
+                IsOpen = _entries.Skip(_lastVersion).Any(t => t.HasHighlight);
+                if (!IsOpen && _lastVersion < _entries.Count)
+                    _setConfig(_entries.Count, ChangeLogDisplayType.Never);
+                break;
+            case ChangeLogDisplayType.Never:
+                IsOpen = false;
+                if (_lastVersion < _entries.Count)
+                    _setConfig(_entries.Count, ChangeLogDisplayType.Never);
+                break;
         }
     }
 
@@ -62,51 +101,90 @@ public sealed class Changelog : Window
 
     public override void Draw()
     {
-        using (var child = ImRaii.Child("Entries", new Vector2(-1, -ImGui.GetFrameHeight() * 2)))
-        {
-            var i = 0;
-            foreach (var ((name, list), idx) in _entries.WithIndex().Reverse())
-            {
-                using var id    = ImRaii.PushId(i++);
-                using var color = ImRaii.PushColor(ImGuiCol.Text, HeaderColor);
-                var       flags = ImGuiTreeNodeFlags.NoTreePushOnOpen;
-                if (idx >= _lastVersion || idx == _entries.Count - 1)
-                    flags |= ImGuiTreeNodeFlags.DefaultOpen;
-
-                var tree = ImGui.TreeNodeEx(name, flags);
-                CopyToClipboard(name, list);
-                color.Pop();
-                if (tree)
-                    foreach (var entry in list)
-                        entry.Draw();
-            }
-        }
-
+        DrawEntries();
         var pos = Size!.Value.X * ImGuiHelpers.GlobalScale / 3;
         ImGui.SetCursorPosX(pos);
-        if (ImGui.Button("Understood", new Vector2(pos, 0)))
+        DrawDisplayTypeCombo(pos);
+        ImGui.SetCursorPosX(pos);
+        DrawUnderstoodButton(pos);
+    }
+
+    private void DrawEntries()
+    {
+        using var child = ImRaii.Child("Entries", new Vector2(-1, -ImGui.GetFrameHeight() * 3));
+        if (!child)
+            return;
+
+        var i = 0;
+        foreach (var ((name, list, hasHighlight), idx) in _entries.WithIndex().Reverse())
         {
-            if (_lastVersion != _entries.Count)
-                _setLastVersion(_entries.Count);
-            ForceOpen = false;
+            using var id    = ImRaii.PushId(i++);
+            using var color = ImRaii.PushColor(ImGuiCol.Text, HeaderColor);
+            var       flags = ImGuiTreeNodeFlags.NoTreePushOnOpen;
+
+
+            // Do open the newest entry if it is the only new entry, if it has highlights or if no highlights are required
+            var isOpen = idx == _entries.Count - 1
+                ? idx == _lastVersion || _displayType != ChangeLogDisplayType.HighlightOnly || hasHighlight
+                // Automatically open all entries that have not been seen, if they have highlights or do not require highlights
+                : idx >= _lastVersion && (hasHighlight || _displayType != ChangeLogDisplayType.HighlightOnly);
+
+            if (isOpen)
+                flags |= ImGuiTreeNodeFlags.DefaultOpen;
+
+            var tree = ImGui.TreeNodeEx(name, flags);
+            CopyToClipboard(name, list);
+            color.Pop();
+            if (!tree)
+                continue;
+
+            foreach (var entry in list)
+                entry.Draw();
         }
+    }
+
+    private void DrawDisplayTypeCombo(float width)
+    {
+        ImGui.SetNextItemWidth(width);
+        using var combo = ImRaii.Combo("##DisplayType", ToName(_displayType));
+        if (!combo)
+            return;
+
+        foreach (var type in Enum.GetValues<ChangeLogDisplayType>())
+        {
+            if (ImGui.Selectable(ToName(type)))
+                _setConfig(_lastVersion, type);
+        }
+    }
+
+    private void DrawUnderstoodButton(float width)
+    {
+        if (!ImGui.Button("Understood", new Vector2(width, 0)))
+            return;
+
+        if (_lastVersion != _entries.Count)
+            _setConfig(_entries.Count, _displayType);
+        ForceOpen = false;
     }
 
     public Changelog NextVersion(string title)
     {
-        _entries.Add((title, new List<Entry>()));
+        _entries.Add((title, new List<Entry>(), false));
         return this;
     }
 
     public Changelog RegisterHighlight(string text, ushort level = 0, uint color = DefaultHighlightColor)
     {
-        _entries.Last().Item2.Add(new Entry(text, color, level));
+        var lastEntry = _entries.Last();
+        lastEntry.Entries.Add(new Entry(text, color, level));
+        if (color != 0)
+            _entries[^1] = lastEntry with { HasHighlight = true };
         return this;
     }
 
     public Changelog RegisterEntry(string text, ushort level = 0)
     {
-        _entries.Last().Item2.Add(new Entry(text, 0, level));
+        _entries.Last().Entries.Add(new Entry(text, 0, level));
         return this;
     }
 
