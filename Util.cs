41581a5d035d46a89819aaefaa8d01c815788eee
Ottermandias <nickroederspam+git1@gmail.com>
2022-09-17 21:44:53 +0200

    Fix shadowed text.

--- Util.cs
+++ Util.cs
@@ -40,9 +40,9 @@ public static partial class ImGuiUtil
         var x = ImGui.GetCursorPosX();
         var y = ImGui.GetCursorPosY();
 
-        for (var i = -shadowWidth; i < shadowWidth; i++)
+        for (var i = -shadowWidth; i <= shadowWidth; i++)
         {
-            for (var j = -shadowWidth; j < shadowWidth; j++)
+            for (var j = -shadowWidth; j <= shadowWidth; j++)
             {
                 if (i == 0 && j == 0)
                     continue;
