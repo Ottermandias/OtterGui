1b61ce894209ebabe6cf2d8b7c120f5a6edbe86a
Ottermandias <nickroederspam+git1@gmail.com>
2022-12-06 15:59:35 +0100

    Small fixes for backup.

--- Classes/Backup.cs
+++ Classes/Backup.cs
@@ -20,16 +20,14 @@ public static class Backup
         try
         {
             var configDirectory = dir.Parent!.FullName;
-            var directory = CreateBackupDirectory(dir);
+            var directory       = CreateBackupDirectory(dir);
             var (newestFile, oldestFile, numFiles) = CheckExistingBackups(directory);
             var newBackupName = Path.Combine(directory.FullName, $"{DateTime.Now:yyyyMMddHHmmss}.zip");
             if (newestFile == null || CheckNewestBackup(newestFile, configDirectory, files.Count))
             {
                 CreateBackup(files, newBackupName, configDirectory);
                 if (numFiles > MaxNumBackups)
-                {
                     oldestFile!.Delete();
-                }
             }
         }
         catch (Exception e)
@@ -42,12 +40,10 @@ public static class Backup
     // Obtain the backup directory. Create it if it does not exist.
     private static DirectoryInfo CreateBackupDirectory(DirectoryInfo dir)
     {
-        var path = Path.Combine(dir.Parent!.Parent!.FullName, "backups", dir.Name);
-        var newDir  = new DirectoryInfo(path);
+        var path   = Path.Combine(dir.Parent!.Parent!.FullName, "backups", dir.Name);
+        var newDir = new DirectoryInfo(path);
         if (!newDir.Exists)
-        {
             newDir = Directory.CreateDirectory(newDir.FullName);
-        }
 
         return newDir;
     }
@@ -57,7 +53,7 @@ public static class Backup
     // Return the newest backup.
     private static (FileInfo? Newest, FileInfo? Oldest, int Count) CheckExistingBackups(DirectoryInfo backupDirectory)
     {
-        var count = 0;
+        var       count  = 0;
         FileInfo? newest = null;
         FileInfo? oldest = null;
 
@@ -66,49 +62,47 @@ public static class Backup
             ++count;
             var time = file.CreationTimeUtc;
             if ((oldest?.CreationTimeUtc ?? DateTime.MaxValue) > time)
-            {
                 oldest = file;
-            }
 
             if ((newest?.CreationTimeUtc ?? DateTime.MinValue) < time)
-            {
                 newest = file;
-            }
         }
 
         return (newest, oldest, count);
     }
 
     // Compare the newest backup against the currently existing files.
-    // If there are any differences, return false, and if they are completely identical, return true.
+    // If there are any differences, return true, and if they are completely identical, return false.
     private static bool CheckNewestBackup(FileInfo newestFile, string configDirectory, int fileCount)
     {
-        using var oldFileStream = File.Open(newestFile.FullName, FileMode.Open);
-        using var oldZip = new ZipArchive(oldFileStream, ZipArchiveMode.Read);
-        // Number of stored files is different.
-        if (fileCount != oldZip.Entries.Count)
+        try
         {
-            return true;
+            using var oldFileStream = File.Open(newestFile.FullName, FileMode.Open);
+            using var oldZip        = new ZipArchive(oldFileStream, ZipArchiveMode.Read);
+            // Number of stored files is different.
+            if (fileCount != oldZip.Entries.Count)
+                return true;
+
+            // Since number of files is identical,
+            // the backups are identical if every file in the old backup
+            // still exists and is identical.
+            foreach (var entry in oldZip.Entries)
+            {
+                var file = Path.Combine(configDirectory, entry.FullName);
+                if (!File.Exists(file))
+                    return true;
+
+                using var currentData = File.OpenRead(file);
+                using var oldData     = entry.Open();
+
+                if (!Equals(currentData, oldData))
+                    return true;
+            }
         }
-
-        // Since number of files is identical,
-        // the backups are identical if every file in the old backup
-        // still exists and is identical.
-        foreach (var entry in oldZip.Entries)
+        catch (Exception e)
         {
-            var file = Path.Combine(configDirectory, entry.FullName);
-            if (!File.Exists(file))
-            {
-                return true;
-            }
-
-            using var currentData = File.OpenRead(file);
-            using var oldData = entry.Open();
-
-            if (!Equals(currentData, oldData))
-            {
-                return true;
-            }
+            PluginLog.Warning($"Could not read the newest backup file {newestFile.FullName}:\n{e}");
+            return true;
         }
 
         return false;
@@ -118,11 +112,9 @@ public static class Backup
     private static void CreateBackup(IEnumerable<FileInfo> files, string fileName, string configDirectory)
     {
         using var fileStream = File.Open(fileName, FileMode.Create);
-        using var zip = new ZipArchive(fileStream, ZipArchiveMode.Create);
+        using var zip        = new ZipArchive(fileStream, ZipArchiveMode.Create);
         foreach (var file in files.Where(f => File.Exists(f.FullName)))
-        {
             zip.CreateEntryFromFile(file.FullName, Path.GetRelativePath(configDirectory, file.FullName), CompressionLevel.Optimal);
-        }
     }
 
     // Compare two streams per byte and return if they are equal.
@@ -131,16 +123,12 @@ public static class Backup
         while (true)
         {
             var current = lhs.ReadByte();
-            var old = rhs.ReadByte();
+            var old     = rhs.ReadByte();
             if (current != old)
-            {
                 return false;
-            }
 
             if (current == -1)
-            {
                 return true;
-            }
         }
     }
 }
