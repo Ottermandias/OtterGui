6969f4c05b2fab57e0dc30ae249be7c23cd81fa4
Ottermandias <nickroederspam+git1@gmail.com>
2023-05-09 17:14:46 +0200

    More options for the FrameworkManager.

--- Classes/FrameworkManager.cs
+++ Classes/FrameworkManager.cs
@@ -7,13 +7,14 @@ using OtterGui.Log;
 
 namespace OtterGui.Classes;
 
-// Manage certain actions to only occur on framework updates.
+/// <summary> Manage certain actions to only occur on framework updates. </summary>
 public class FrameworkManager : IDisposable
 {
-    private readonly Logger                     _log;
-    private readonly Framework                  _framework;
-    private readonly Dictionary<string, Action> _important = new();
-    private readonly Dictionary<string, Action> _delayed   = new();
+    private readonly Logger                                 _log;
+    private readonly Framework                              _framework;
+    private readonly Dictionary<string, Action>             _important = new();
+    private readonly Dictionary<string, Action>             _onTick    = new();
+    private readonly LinkedList<(DateTime, string, Action)> _delayed   = new();
 
     public FrameworkManager(Framework framework, Logger log)
     {
@@ -22,20 +23,57 @@ public class FrameworkManager : IDisposable
         _framework.Update += OnUpdate;
     }
 
-    // Register an action that is not time critical.
-    // One action per frame will be executed.
-    // On dispose, any remaining actions will be executed.
-    public void RegisterDelayed(string tag, Action action)
+    public List<string> Important
     {
-        lock (_delayed)
+        get
         {
-            _delayed[tag] = action;
+            lock (_important)
+            {
+                return _important.Keys.ToList();
+            }
         }
     }
 
-    // Register an action that should be executed on the next frame.
-    // All of those actions will be executed in the next frame.
-    // If there are more than one, they will be launched in separated tasks, but waited for.
+    public List<string> OnTick
+    {
+        get
+        {
+            lock (_onTick)
+            {
+                return _onTick.Keys.ToList();
+            }
+        }
+    }
+
+    public List<(DateTime, string)> Delayed
+    {
+        get
+        {
+            lock (_delayed)
+            {
+                return _delayed.Select(t => (t.Item1, t.Item2)).ToList();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Register an action that is not time critical.
+    /// One action per frame will be executed.
+    /// On dispose, any remaining actions will be executed.
+    /// </summary>
+    public void RegisterOnTick(string tag, Action action)
+    {
+        lock (_onTick)
+        {
+            _onTick[tag] = action;
+        }
+    }
+
+    /// <summary>
+    /// Register an action that should be executed on the next frame.
+    /// All of those actions will be executed in the next frame.
+    /// If there are more than one, they will be launched in separated tasks, but waited for.
+    /// </summary>
     public void RegisterImportant(string tag, Action action)
     {
         lock (_important)
@@ -44,12 +82,63 @@ public class FrameworkManager : IDisposable
         }
     }
 
+
+    /// <summary>
+    /// Register an action that is expected to be delayed.
+    /// One action per frame will be executed when the delay has been waited for.
+    /// On dispose, any remaining actions will be executed.
+    /// If the action is already registered and the desired time is earlier, it will be updated,
+    /// if it is later, it will be ignored.
+    /// </summary>
+    public void RegisterDelayed(string tag, Action action, TimeSpan delay)
+    {
+        var desiredTime = DateTime.UtcNow + delay;
+        lock (_delayed)
+        {
+            var node = _delayed.First;
+            if (node == null)
+            {
+                _delayed.AddFirst((desiredTime, tag, action));
+                return;
+            }
+
+            LinkedListNode<(DateTime, string, Action)>? delete    = null;
+            LinkedListNode<(DateTime, string, Action)>? addBefore = null;
+            while (node != null)
+            {
+                if (delete == null && node.Value.Item2 == tag)
+                {
+                    if (node.Value.Item1 < desiredTime)
+                        return;
+
+                    delete = node;
+                }
+
+                if (addBefore == null && node.Value.Item1 > desiredTime)
+                    addBefore = node;
+                node = node.Next;
+            }
+
+            if (addBefore != null)
+                _delayed.AddBefore(addBefore, (desiredTime, tag, action));
+            else
+                _delayed.AddLast((desiredTime, tag, action));
+
+            if (delete != null)
+                _delayed.Remove(delete);
+        }
+    }
+
     public void Dispose()
     {
         _framework.Update -= OnUpdate;
-        foreach (var (_, action) in _delayed)
+        foreach (var (_, action) in _onTick)
             action();
 
+        _onTick.Clear();
+
+        foreach (var (_, _, action) in _delayed)
+            action();
         _delayed.Clear();
     }
 
@@ -57,8 +146,9 @@ public class FrameworkManager : IDisposable
     {
         try
         {
-            HandleOne(_delayed);
+            HandleOnTick(_onTick);
             HandleAllTasks(_important);
+            HandleDelayed(_delayed);
         }
         catch (Exception e)
         {
@@ -66,7 +156,26 @@ public class FrameworkManager : IDisposable
         }
     }
 
-    private static void HandleOne(IDictionary<string, Action> dict)
+    private static void HandleDelayed(LinkedList<(DateTime, string, Action)> list)
+    {
+        if (list.Count == 0)
+            return;
+
+        var                                         now = DateTime.UtcNow;
+        LinkedListNode<(DateTime, string, Action)>? node;
+        lock (list)
+        {
+            node = list.First;
+            if (node != null && node.Value.Item1 < now)
+                list.RemoveFirst();
+            else
+                node = null;
+        }
+
+        node?.Value.Item3.Invoke();
+    }
+
+    private static void HandleOnTick(IDictionary<string, Action> dict)
     {
         if (dict.Count == 0)
             return;
@@ -85,7 +194,7 @@ public class FrameworkManager : IDisposable
     {
         if (dict.Count < 2)
         {
-            HandleOne(dict);
+            HandleOnTick(dict);
         }
         else
         {
