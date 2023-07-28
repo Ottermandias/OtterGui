03b6b17fee66488fff7f598e444fa99454098767
Ottermandias <nickroederspam+git1@gmail.com>
2023-07-28 18:21:17 +0200

    Add Transformlist.

---
+++ Classes/TransformList.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OtterGui.Classes;
+
+/// <summary>
+/// A IReadOnlyList based on any other IReadOnlyList that applies a transforming step before fetching.
+/// </summary>
+public readonly struct TransformList<TIn, TOut> : IReadOnlyList<TOut>
+{
+    private readonly IReadOnlyList<TIn> _list;
+    private readonly Func<TIn, TOut>    _transform;
+
+    public TransformList(IReadOnlyList<TIn> items, Func<TIn, TOut> transform)
+    {
+        _list      = items;
+        _transform = transform;
+    }
+
+    public IEnumerator<TOut> GetEnumerator()
+        => _list.Select(_transform).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+
+    public int Count
+        => _list.Count;
+
+    public TOut this[int index]
+        => _transform(_list[index]);
+}
