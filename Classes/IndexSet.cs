1e172ee9a0f5946d67b848a36b2be97f6541453f
Exter-N <exter-n@exter-n.fr>
2023-08-24 05:18:32 +0200

    IndexSet: Add ranges and complement enumerators.

--- Classes/IndexSet.cs
+++ Classes/IndexSet.cs
@@ -81,16 +81,22 @@ public class IndexSet : IEnumerable<int>
         return ret;
     }
 
-    public IEnumerator<int> GetEnumerator()
+    /// <summary>
+    /// Gets an enumerable that will return the indices that are either part of this set, or missing from it.
+    /// </summary>
+    /// <param name="complement">false (default) to get the indices that are part of this set, true to get those that are missing from it</param>
+    /// <returns>The index enumerable</returns>
+    public IEnumerable<int> Indices(bool complement = false)
     {
-        if (_count <= 0)
+        var capacity = _set.Count;
+        var remaining = complement ? capacity - _count : _count;
+
+        if (remaining <= 0)
             yield break;
 
-        var capacity  = _set.Count;
-        var remaining = _count;
         for (var i = 0; i < capacity; ++i)
         {
-            if (!_set[i])
+            if (_set[i] == complement)
                 continue;
 
             yield return i;
@@ -100,6 +106,39 @@ public class IndexSet : IEnumerable<int>
         }
     }
 
+    public IEnumerator<int> GetEnumerator()
+        => Indices().GetEnumerator();
+
     IEnumerator IEnumerable.GetEnumerator()
-        => GetEnumerator();
+        => Indices().GetEnumerator();
+
+    /// <summary>
+    /// Gets an enumerable that will return the ranges of indices that are either part of this set, or missing from it.
+    /// </summary>
+    /// <param name="complement">false (default) to get the ranges of indices that are part of this set, true to get those that are missing from it</param>
+    /// <returns>The range enumerable</returns>
+    public IEnumerable<(int Start, int End)> Ranges(bool complement = false)
+    {
+        var capacity = _set.Count;
+        var remaining = complement ? capacity - _count : _count;
+
+        if (remaining <= 0)
+            yield break;
+
+        for (var i = 0; i < capacity; ++i)
+        {
+            if (_set[i] == complement)
+                continue;
+
+            var start = i;
+            while (i < capacity && _set[i] != complement)
+                ++i;
+
+            yield return (start, i);
+
+            remaining -= i - start;
+            if (remaining == 0)
+                yield break;
+        }
+    }
 }
