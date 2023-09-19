4ea1eb7b7d39a74465291164563ad5d29fa038e4
Ottermandias <nickroederspam+git1@gmail.com>
2023-09-19 20:37:03 +0200

    Add DayChangeTracker.

---
+++ Classes/DayChangeTracker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Timers;
+
+namespace OtterGui.Classes;
+
+public static class DayChangeTracker
+{
+    private static readonly Timer Timer = new(GetSleepTime());
+
+    public static event Action<int, int, int>? DayChanged;
+
+    static DayChangeTracker()
+    {
+        Timer.Elapsed += (s, e) =>
+        {
+            var now = DateTime.Now;
+            DayChanged?.Invoke(now.Day, now.Month, now.Year);
+            Timer.Interval = GetSleepTime();
+        };
+        Timer.Start();
+    }
+
+    private static double GetSleepTime()
+    {
+        var midnightTonight          = DateTime.Today.AddDays(1);
+        var differenceInMilliseconds = (midnightTonight - DateTime.Now).TotalMilliseconds;
+        return differenceInMilliseconds;
+    }
+
+    private static void OnSystemTimeChanged(object sender, EventArgs e)
+    {
+        Timer.Interval = GetSleepTime();
+    }
+}
