f624aca526bd1f36364d63443ed1c6e83499b8b7
Ottermandias <nickroederspam+git1@gmail.com>
2023-12-01 16:03:21 +0100

    Add InMethodChecker.

---
+++ Classes/InMethodChecker.cs
@@ -0,0 +1,36 @@
+﻿namespace OtterGui.Classes;
+
+public readonly struct InMethodChecker
+{
+    private readonly ThreadLocal<uint> _inUpdate = new(() => 0, false);
+
+    public uint RecursionDepth
+        => _inUpdate.IsValueCreated ? _inUpdate.Value : 0;
+
+    public bool InMethod
+        => _inUpdate is { IsValueCreated: true, Value: > 0 };
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public RaiiSetter EnterMethod()
+        => new(_inUpdate);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public InMethodChecker()
+    { }
+
+    public readonly ref struct RaiiSetter
+    {
+        private readonly ThreadLocal<uint> _threadLocal;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public RaiiSetter(ThreadLocal<uint> threadLocal)
+        {
+            _threadLocal = threadLocal;
+            ++_threadLocal.Value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        public void Dispose()
+            => --_threadLocal.Value;
+    }
+}
