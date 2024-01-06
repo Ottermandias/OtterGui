e4a82619332aade2748a381956a1ab8934de6211
Ottermandias <nickroederspam+git1@gmail.com>
2024-01-06 23:56:10 +0100

    Add some wrappers

--- Classes/EventWrapperBase.cs
+++ Classes/EventWrapperBase.cs
@@ -12,6 +12,10 @@ public static class EventWrapperBase
 }
 
 public delegate        void ActionRef<T1>(ref T1 a);
+public delegate        void ActionRef<T1, T2>(ref T1 a, ref T2 b);
+public delegate        void ActionRef2<in T1, T2>(T1 a, ref T2 b);
+public delegate        void ActionRef3<in T1, in T2, T3>(T1 a, T2 b, ref T3 c);
+public delegate        void ActionRef34<in T1, in T2, T3, T4>(T1 a, T2 b, ref T3 c, ref T4 d);
 public unsafe delegate void ActionPtr<T1>(T1* a) where T1 : unmanaged;
 public unsafe delegate void ActionPtr<T1, T2>(T1* a, T2* b) where T1 : unmanaged where T2 : unmanaged;
 public unsafe delegate void ActionPtr<T1, T2, T3>(T1* a, T2* b, T3* c) where T1 : unmanaged where T2 : unmanaged where T3 : unmanaged;
@@ -86,7 +90,8 @@ public abstract class EventWrapperBase<TPriority>(string name) : IDisposable, IS
     {
         Lock.EnterReadLock();
         for (var i = Event.Count - 1; i >= 0; --i)
-                yield return (T)Event[i].Subscriber;
+            yield return (T)Event[i].Subscriber;
+
         Lock.ExitReadLock();
     }
 }
@@ -257,6 +262,81 @@ public abstract class EventWrapper<T1, T2, T3, T4, T5, T6, TPriority>(string nam
             try
             {
                 action.Invoke(a, b, c, d, e, f);
+            }
+            catch (Exception ex)
+            {
+                EventWrapperBase.Logger?.Error($"[{Name}] Exception thrown during invocation:\n{ex}");
+            }
+        }
+    }
+}
+
+public abstract unsafe class EventWrapperRef2<T1, T2, TPriority>(string name) : EventWrapperBase<TPriority>(name)
+    where TPriority : struct, Enum
+{
+    public void Subscribe(ActionRef2<T1, T2> subscriber, TPriority priority)
+        => base.Subscribe(subscriber, priority);
+
+    public void Unsubscribe(ActionRef2<T1, T2> subscriber)
+        => base.Unsubscribe(subscriber);
+
+    public void Invoke(T1 a, ref T2 b)
+    {
+        foreach (var action in Enumerate<ActionRef2<T1, T2>>())
+        {
+            try
+            {
+                action.Invoke(a, ref b);
+            }
+            catch (Exception ex)
+            {
+                EventWrapperBase.Logger?.Error($"[{Name}] Exception thrown during invocation:\n{ex}");
+            }
+        }
+    }
+}
+
+public abstract unsafe class EventWrapperRef3<T1, T2, T3, TPriority>(string name) : EventWrapperBase<TPriority>(name)
+    where TPriority : struct, Enum
+{
+    public void Subscribe(ActionRef3<T1, T2, T3> subscriber, TPriority priority)
+        => base.Subscribe(subscriber, priority);
+
+    public void Unsubscribe(ActionRef3<T1, T2, T3> subscriber)
+        => base.Unsubscribe(subscriber);
+
+    public void Invoke(T1 a, T2 b, ref T3 c)
+    {
+        foreach (var action in Enumerate<ActionRef3<T1, T2, T3>>())
+        {
+            try
+            {
+                action.Invoke(a, b, ref c);
+            }
+            catch (Exception ex)
+            {
+                EventWrapperBase.Logger?.Error($"[{Name}] Exception thrown during invocation:\n{ex}");
+            }
+        }
+    }
+}
+
+public abstract unsafe class EventWrapperRef34<T1, T2, T3, T4, TPriority>(string name) : EventWrapperBase<TPriority>(name)
+    where TPriority : struct, Enum
+{
+    public void Subscribe(ActionRef34<T1, T2, T3, T4> subscriber, TPriority priority)
+        => base.Subscribe(subscriber, priority);
+
+    public void Unsubscribe(ActionRef34<T1, T2, T3, T4> subscriber)
+        => base.Unsubscribe(subscriber);
+
+    public void Invoke(T1 a, T2 b, ref T3 c, ref T4 d)
+    {
+        foreach (var action in Enumerate<ActionRef34<T1, T2, T3, T4>>())
+        {
+            try
+            {
+                action.Invoke(a, b, ref c, ref d);
             }
             catch (Exception ex)
             {
