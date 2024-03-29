03125ef160152db5bf9e0dd90c53288a6dce0e91
Ottermandias <nickroederspam+git1@gmail.com>
2024-03-29 14:43:45 +0100

    Add MoveBit and RemoveBit for ulong.

--- Functions.cs
+++ Functions.cs
@@ -88,6 +88,17 @@ public static class Functions
         return low | high;
     }
 
+    // Remove a single bit, moving all further bits one down.
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static ulong RemoveBit(ulong config, int bit)
+    {
+        var lowMask  = (1ul << bit) - 1ul;
+        var highMask = ~((1ul << (bit + 1)) - 1ul);
+        var low      = config & lowMask;
+        var high     = (config & highMask) >> 1;
+        return low | high;
+    }
+
     // Move a bit in an uint from its position to another, shifting other bits accordingly.
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static uint MoveBit(uint config, int bit1, int bit2)
@@ -95,6 +106,18 @@ public static class Functions
         var enabled = (config & (1 << bit1)) != 0 ? 1u << bit2 : 0u;
         config = RemoveBit(config, bit1);
         var lowMask = (1u << bit2) - 1u;
+        var low     = config & lowMask;
+        var high    = (config & ~lowMask) << 1;
+        return low | enabled | high;
+    }
+
+    // Move a bit in an uint from its position to another, shifting other bits accordingly.
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static ulong MoveBit(ulong config, int bit1, int bit2)
+    {
+        var enabled = (config & (1ul << bit1)) != 0 ? 1ul << bit2 : 0ul;
+        config = RemoveBit(config, bit1);
+        var lowMask = (1ul << bit2) - 1ul;
         var low     = config & lowMask;
         var high    = (config & ~lowMask) << 1;
         return low | enabled | high;
