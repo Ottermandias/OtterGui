801d71b40416c9e165473e6b4235ff2418ec38e5
Ottermandias <nickroederspam+git1@gmail.com>
2024-07-31 22:51:43 +0200

    Add TextColumn and FrameColumn to ImUtf8.

---
+++ Text/Extended/ImUtf8.TextColumn.cs
@@ -0,0 +1,66 @@
+using ImGuiNET;
+using OtterGui.Text.HelperObjects;
+
+namespace OtterGui.Text;
+
+#pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
+
+public static partial class ImUtf8
+{
+    /// <summary> Go to the next table column and draw text. </summary>
+    /// <param name="text"> The given text as a UTF8 string. Does not have to be null-terminated. </param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawTableColumn(ReadOnlySpan<byte> text)
+    {
+        ImGui.TableNextColumn();
+        Text(text);
+    }
+
+    /// <param name="text"> The given text as a UTF16 string. </param>
+    /// <inheritdoc cref="DrawTableColumn(ReadOnlySpan{byte})"/>
+    /// <exception cref="ImUtf8FormatException" />
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawTableColumn(ReadOnlySpan<char> text)
+    {
+        ImGui.TableNextColumn();
+        Text(text);
+    }
+
+    /// <param name="text"> The given text as a formatted string. </param>
+    /// <inheritdoc cref="DrawTableColumn(ReadOnlySpan{char})"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawTableColumn(ref Utf8StringHandler<TextStringHandlerBuffer> text)
+    {
+        ImGui.TableNextColumn();
+        Text(ref text);
+    }
+
+
+    /// <summary> Go to the next table column and draw frame-aligned text. </summary>
+    /// <param name="text"> The given text as a UTF8 string. Does not have to be null-terminated. </param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawFrameColumn(ReadOnlySpan<byte> text)
+    {
+        ImGui.TableNextColumn();
+        TextFrameAligned(text);
+    }
+
+    /// <param name="text"> The given text as a UTF16 string. </param>
+    /// <inheritdoc cref="DrawTableColumn(ReadOnlySpan{byte})"/>
+    /// <exception cref="ImUtf8FormatException" />
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawFrameColumn(ReadOnlySpan<char> text)
+    {
+        ImGui.TableNextColumn();
+        TextFrameAligned(text);
+    }
+
+    /// <param name="text"> The given text as a formatted string. </param>
+    /// <inheritdoc cref="DrawTableColumn(ReadOnlySpan{char})"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static void DrawFrameColumn(ref Utf8StringHandler<TextStringHandlerBuffer> text)
+    {
+        ImGui.TableNextColumn();
+        TextFrameAligned(ref text);
+    }
+}
