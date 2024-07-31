f99a6c01c3050cfe128787a92a34ca5f7d8525c9
Ottermandias <nickroederspam+git1@gmail.com>
2024-07-31 22:04:43 +0200

    Add doc to ListClipper.

--- Text/EndObjects/ListClipper.cs
+++ Text/EndObjects/ListClipper.cs
@@ -2,35 +2,43 @@ using ImGuiNET;
 
 namespace OtterGui.Text.EndObjects;
 
+/// <summary> A clipper utility that cleans up after itself. </summary>
 public ref struct ListClipper
 {
+    /// <summary> The clipper itself. </summary>
     public readonly ImGuiListClipperPtr ClipperPtr;
-    public          bool                Disposed;
 
+    public bool Disposed { get; private set; }
+
+    /// <summary> The index of the first displayed element. </summary>
     public readonly ref int DisplayStart
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => ref ClipperPtr.DisplayStart;
     }
 
+    /// <summary> The index of the last displayed element. </summary>
     public readonly ref int DisplayEnd
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => ref ClipperPtr.DisplayEnd;
     }
 
+    /// <summary> The number of displayed items. </summary>
     public readonly ref int ItemsCount
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => ref ClipperPtr.ItemsCount;
     }
 
+    /// <summary> The total height of the items. </summary>
     public readonly ref float ItemsHeight
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get => ref ClipperPtr.ItemsHeight;
     }
 
+    /// <summary> The height offset of the skipped height. </summary>
     public readonly ref float StartPosY
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -44,22 +52,25 @@ public ref struct ListClipper
         ClipperPtr.Begin(itemsCount, itemsHeight);
     }
 
+    /// <summary> Force the current display range using a pair of indices. </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void ForceDisplayRangeByIndices(int itemMin, int itemMax)
-        => ClipperPtr.ForceDisplayRangeByIndices(itemMin, itemMax);
+    public unsafe void ForceDisplayRangeByIndices(int itemMin, int itemMax)
+        => ImGuiNative.ImGuiListClipper_ForceDisplayRangeByIndices(ClipperPtr.NativePtr, itemMin, itemMax);
 
+    /// <summary> Execute a step in the clipper. </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public bool Step()
-        => ClipperPtr.Step();
+    public unsafe bool Step()
+        => ImGuiNative.ImGuiListClipper_Step(ClipperPtr.NativePtr) != 0;
 
+    /// <summary> Dispose of the list clipper. </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Dispose()
+    public unsafe void Dispose()
     {
         if (Disposed)
             return;
 
-        ClipperPtr.End();
-        ClipperPtr.Destroy();
+        ImGuiNative.ImGuiListClipper_End(ClipperPtr.NativePtr);
+        ImGuiNative.ImGuiListClipper_destroy(ClipperPtr.NativePtr);
         Disposed = true;
     }
 }
