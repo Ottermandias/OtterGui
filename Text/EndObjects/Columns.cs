1bc55ac8660882c48f14b5096ad7ad45c490a484
Ottermandias <nickroederspam+git1@gmail.com>
2024-07-31 15:56:43 +0200

    Cleanup Columns.

--- Text/EndObjects/Columns.cs
+++ Text/EndObjects/Columns.cs
@@ -5,18 +5,15 @@ namespace OtterGui.Text.EndObjects;
 
 public readonly ref struct Columns
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public unsafe Columns(int count, ReadOnlySpan<byte> id = default, bool border = true)
-    {
-        ImGuiNative.igColumns(count, id.Start(), border.Byte());
-    }
+        => ImGuiNative.igColumns(count, id.Start(), border.Byte());
 
-    public readonly void Next()
-    {
-        ImGuiNative.igNextColumn();
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public void Next()
+        => ImGuiNative.igNextColumn();
 
-    public readonly unsafe void Dispose()
-    {
-        ImGuiNative.igColumns(1, null, 1);
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public unsafe void Dispose()
+        => ImGuiNative.igColumns(1, null, 1);
 }
