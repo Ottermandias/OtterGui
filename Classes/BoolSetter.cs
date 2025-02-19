06422a893348a18a013e6dbc558370db8d21a446
Ottermandias <nickroederspam+git1@gmail.com>
2025-02-19 23:36:14 +0100

    Add BoolSetter.

---
+++ Classes/BoolSetter.cs
@@ -0,0 +1,19 @@
+﻿namespace OtterGui.Classes;
+
+public readonly ref struct BoolSetter
+{
+    private readonly ref bool _value;
+
+    [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.AggressiveInlining)]
+    public BoolSetter(ref bool value, bool setTo = true)
+    {
+        _value = ref value;
+        _value = setTo;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+    {
+        _value = false;
+    }
+}
