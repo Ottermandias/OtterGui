aff825984c6b835c8b03cb2fdf9dfbfcfb65f4ff
Ottermandias <nickroederspam+git1@gmail.com>
2025-05-13 17:10:36 +0200

    Update some table code and add TriStateColumnFlags.

--- Table/ColumnFlags.cs
+++ Table/ColumnFlags.cs
@@ -1,6 +1,8 @@
 using Dalamud.Interface.Utility;
 using ImGuiNET;
 using OtterGui.Raii;
+using OtterGui.Text;
+using OtterGui.Text.Widget;
 
 namespace OtterGui.Table;
 
@@ -24,17 +26,91 @@ public class ColumnFlags<T, TItem> : Column<TItem> where T : struct, Enum
     protected virtual bool DrawCheckbox(int idx, out bool ret)
     {
         ret = FilterValue.HasFlag(Values[idx]);
-        return ImGui.Checkbox(Names[idx], ref ret);
+        return ImUtf8.Checkbox(Names[idx], ref ret);
     }
 
     public override bool DrawFilter()
     {
-        using var id    = ImRaii.PushId(FilterLabel);
+        using var id    = ImUtf8.PushId(FilterLabel);
         using var style = ImRaii.PushStyle(ImGuiStyleVar.FrameRounding, 0);
         ImGui.SetNextItemWidth(-Table.ArrowWidth * ImGuiHelpers.GlobalScale);
         var       all   = FilterValue.HasFlag(AllFlags);
         using var color = ImRaii.PushColor(ImGuiCol.FrameBg, 0x803030A0, !all);
-        using var combo = ImRaii.Combo(string.Empty, Label, ComboFlags);
+        using var combo = ImUtf8.Combo(""u8, Label, ComboFlags);
+
+        if (ImGui.IsItemClicked(ImGuiMouseButton.Right))
+        {
+            SetValue(AllFlags, true);
+            return true;
+        }
+
+        if (!all)
+            ImUtf8.HoverTooltip("Right-click to clear filters."u8);
+
+        if (!combo)
+            return false;
+
+        color.Pop();
+
+        var ret = false;
+        if (ImUtf8.Checkbox("Enable All"u8, ref all))
+        {
+            SetValue(AllFlags, all);
+            ret = true;
+        }
+
+        using var indent = ImRaii.PushIndent(10f);
+        for (var i = 0; i < Names.Length; ++i)
+        {
+            if (!DrawCheckbox(i, out var tmp))
+                continue;
+
+            SetValue(Values[i], tmp);
+            ret = true;
+        }
+
+        return ret;
+    }
+}
+
+public abstract class TriStateColumnFlags<T, TItem> : Column<TItem> where T : struct, Enum
+{
+    public    T               AllFlags   = default;
+    protected ImGuiComboFlags ComboFlags = ImGuiComboFlags.NoArrowButton;
+
+    protected abstract IReadOnlyList<(T On, T Off)> Values { get; }
+
+    protected abstract string[] Names { get; }
+
+    public virtual T FilterValue
+        => default;
+
+    protected virtual void SetValue((T On, T Off) value, bool? enable)
+    { }
+
+    protected virtual void SetValue(T flags, bool value)
+    { }
+
+    protected virtual bool DrawCheckbox(int idx, out bool? ret)
+    {
+        var (on, off) = Values[idx];
+        bool? current = FilterValue.HasFlag(on)
+            ? FilterValue.HasFlag(off)
+                ? null
+                : true
+            : false;
+
+        return TriStateCheckbox.Instance.Draw(Names[idx], current, out ret);
+    }
+
+    public override bool DrawFilter()
+    {
+        using var id    = ImUtf8.PushId(FilterLabel);
+        using var style = ImRaii.PushStyle(ImGuiStyleVar.FrameRounding, 0);
+        ImGui.SetNextItemWidth(-Table.ArrowWidth * ImGuiHelpers.GlobalScale);
+        var       all   = FilterValue.HasFlag(AllFlags);
+        using var color = ImRaii.PushColor(ImGuiCol.FrameBg, 0x803030A0, !all);
+        using var combo = ImUtf8.Combo(""u8, Label, ComboFlags);
 
         if (ImGui.IsItemClicked(ImGuiMouseButton.Right))
         {
@@ -43,7 +119,7 @@ public class ColumnFlags<T, TItem> : Column<TItem> where T : struct, Enum
         }
 
         if (!all && ImGui.IsItemHovered())
-            ImGui.SetTooltip("Right-click to clear filters.");
+            ImUtf8.HoverTooltip("Right-click to clear filters."u8);
 
         if (!combo)
             return false;
@@ -51,13 +127,6 @@ public class ColumnFlags<T, TItem> : Column<TItem> where T : struct, Enum
         color.Pop();
 
         var ret = false;
-        if (ImGui.Checkbox("Enable All", ref all))
-        {
-            SetValue(AllFlags, all);
-            ret = true;
-        }
-
-        using var indent = ImRaii.PushIndent(10f);
         for (var i = 0; i < Names.Length; ++i)
         {
             if (!DrawCheckbox(i, out var tmp))
