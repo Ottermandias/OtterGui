00cd7dc9d679e896dd27829e9b91561f58a30132
Haselnussbomber <mail@haselnussbomber.de>
2025-08-02 00:56:25 +0200

    Fix weird encoding issue and formatting

--- Text/ImUtf8.ListClipper.cs
+++ Text/ImUtf8.ListClipper.cs
@@ -5,7 +5,5 @@ namespace OtterGui.Text;
 public static partial class ImUtf8
 {
     public static ListClipper ListClipper(int itemsCount, float itemsHeight = -1f)
-    {
-        return new(itemsCount, itemsHeight);
-    }
+        => new(itemsCount, itemsHeight);
 }
--- Text/ImUtf8.RotatedText.cs
+++ Text/ImUtf8.RotatedText.cs
@@ -7,7 +7,7 @@ namespace OtterGui.Text;
 #pragma warning disable CS1573
 public static unsafe partial class ImUtf8
 {
-    /// <summary> Draw text rotated by 90�. </summary>
+    /// <summary> Draw text rotated by 90°. </summary>
     /// <param name="text"> The given text as a UTF8 string. Does not have to be null-terminated. </param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void RotatedText(ReadOnlySpan<byte> text, bool alignToFrame = false)
